cb5c14850f0e5dc71f57aaa1bb67bdc579cd02ca
Hoa <ngochoa.bk14@gmail.com>
2021-06-08 10:40:28 +0700

    fix error Nhi's section

--- js/dark-theme.js
+++ js/dark-theme.js
@@ -1,5 +1,10 @@
-document.getElementById("toggleBtn").onclick = function () {
+var getThemeElement = document.getElementById("toggleBtn");
+getThemeElement.onclick = function () {
   var bodyTag = document.getElementById("mybody");
   console.log(bodyTag);
   bodyTag.classList.toggle("dark");
+
+  // change icon
+  getThemeElement.classList.toggle("fa-sun");
 };
+//<i class="fa fa-sun"></i>
